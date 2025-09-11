a47ea0fb3a680ac703f63f4666b998bd4919acab
Violas <gabrielviolante000@gmail.com>
2025-09-11 08:42:29 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/senha.py
@@ -0,0 +1,18 @@
+#senha_correta = "1818"  
+
+#while True:
+    #senha = input("Digite a senha: ")
+    
+   # if senha == senha_correta:
+        #print(" Senha correta! Acesso liberado.")
+        #break
+     
+    #else:
+        #print("Senha incorreta, tente novamente.")
+        
+senha_correta = "1818"
+
+while input('Digite a senha: ') != senha_correta:
+    print("Senha incorreta, Tente novamente.")
+
+print("Senha correta, Acesso liberado")
