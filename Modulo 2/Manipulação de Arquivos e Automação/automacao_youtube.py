8e688aa00f309850415d0cc4a3756f5d4873b5b7
Violas <gabrielviolante000@gmail.com>
2025-12-02 09:08:10 -0300

    Add files via upload

---
+++ Modulo 2/Manipulação de Arquivos e Automação/automacao_youtube.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+#pg.mouseInfo()
+
+pg.press('win')
+pg.write('youtube.com')
+pg.click(696, 483)
+pg.moveTo(698,101)
+pg.sleep(seconds= 2)
+pg.click()
+pg.write('yuri dinamite')
+pg.click(1206,94)
+pg.sleep(seconds= 2)
+pg.click(622,797)
